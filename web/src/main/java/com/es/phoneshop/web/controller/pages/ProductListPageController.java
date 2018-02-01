9e50c6bc4211a1de6d60056b667b3e6126245eff
alastifer <grishkevichaaa@gmail.com>
2018-02-01 11:40:14 +0300

    Changed controller for phones list. Added attribute for count of page

--- web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
@@ -8,16 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.es.core.model.phone.PhoneDao;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping (value = "/productList")
 public class ProductListPageController {
+
     @Resource
     private PhoneDao phoneDao;
 
+    private static final int AMOUNT_PRODUCTS_ON_PAGE = 10;
+
+    private static final String ATTRIBUTE_PHONES_LIST = "phones";
+
+    private static final String ATTRIBUTE_PAGE_COUNT = "pageCount";
+
     @RequestMapping(method = RequestMethod.GET)
-    public String showProductList(Model model) {
-        model.addAttribute("phones", phoneDao.findAll(0, 10));
+    public String showProductList(Model model,
+                                  @RequestParam(value = "page", defaultValue = "1") int page,
+                                  @RequestParam(value = "order", defaultValue = "brand, model asc") String orderBy) {
+
+        model.addAttribute(ATTRIBUTE_PHONES_LIST, phoneDao.findAllInOrder(orderBy,
+                                                                          AMOUNT_PRODUCTS_ON_PAGE,
+                                                                          AMOUNT_PRODUCTS_ON_PAGE * (page - 1)));
+        model.addAttribute(ATTRIBUTE_PAGE_COUNT, (int) Math.ceil(phoneDao.productsCount() / (double) AMOUNT_PRODUCTS_ON_PAGE));
         return "productList";
     }
+
 }
