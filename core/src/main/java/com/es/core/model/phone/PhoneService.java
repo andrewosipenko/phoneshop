4aa64468a5cf57a54071e113f6a8cd14b17af5ce
alastifer <grishkevichaaa@gmail.com>
2018-02-13 17:23:49 +0300

    Created service for call DAO

---
+++ core/src/main/java/com/es/core/model/phone/PhoneService.java
@@ -0,0 +1,12 @@
+package com.es.core.model.phone;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PhoneService {
+    Optional<Phone> get(Long key);
+    List<Phone> findInOrder(String orderBy, int offset, int limit);
+    List<Phone> findByModelInOrder(String model, String orderBy, int offset, int limit);
+    long productsCount();
+    long productsCountByModel(String model);
+}
