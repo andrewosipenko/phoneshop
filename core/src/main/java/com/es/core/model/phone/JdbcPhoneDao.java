d67f79b2e41c3f63a5d448dccf4a6c3035605376
Evgeniy Dragalev <evg.dragalev@gmail.com>
2018-02-20 21:58:46 +0300

    Task 2.1: DAO
    Implement com.es.core.model.JdbcPhoneDao to load product information including colors

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -5,16 +5,46 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Component
 public class JdbcPhoneDao implements PhoneDao{
     @Resource
     private JdbcTemplate jdbcTemplate;
 
+    private final static String SQL_GET_PHONE_QUERY = "SELECT * FROM phones WHERE id=?";
+    private final static String SQL_GET_COLORS_QUERY = "SELECT code" +
+                                                        "FROM colors LEFT JOIN phone2color ON colors.id = phone2color.colorID" +
+                                                        "WHERE phoneID=?";
+
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        Phone phone = jdbcTemplate.queryForObject(
+                SQL_GET_PHONE_QUERY,
+                new Object[]{key},
+                new BeanPropertyRowMapper<>(Phone.class)
+        );
+
+        if (phone != null){
+            phone.setColors(queryColors(key));
+        }
+
+        return Optional.of(phone);
+    }
+
+    private Set<Color> queryColors(final Long key) {
+        return new HashSet<>(jdbcTemplate.query(
+                SQL_GET_COLORS_QUERY,
+                new Object[]{key},
+                (resultSet, i) -> {
+                    Color color = new Color();
+                    color.setCode(resultSet.getString("code"));
+                    color.setId(resultSet.getLong("id"));
+                    return color;
+                }
+        ));
     }
 
     public void save(final Phone phone) {
