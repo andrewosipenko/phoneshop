22fb3064e0f838298adc6b454edea332e7024943
Evgeniy Dragalev <evg.dragalev@gmail.com>
2018-02-25 18:06:04 +0300

    Task 2.1: DAO
    Implement JdbcPhoneDao.save() method, move phone querying to separate method

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -16,39 +16,118 @@ public class JdbcPhoneDao implements PhoneDao{
     private JdbcTemplate jdbcTemplate;
 
     private final static String SQL_GET_PHONE_QUERY = "SELECT * FROM phones WHERE id=?";
-    private final static String SQL_GET_COLORS_QUERY = "SELECT code" +
-                                                        "FROM colors LEFT JOIN phone2color ON colors.id = phone2color.colorID" +
-                                                        "WHERE phoneID=?";
+
+    private final static String SQL_GET_COLORS_QUERY =
+            "SELECT colors.id, colors.code " +
+            "FROM colors, phone2color " +
+            "WHERE phone2color.colorId = colors.id AND phone2color.phoneId=?";
+
+    private final static String SQL_INSERT_PHONE_IF_NOT_EXIST_QUERY =
+            "INSERT INTO phones (id, brand, model) " +
+            "SELECT ?, ?, ? " +
+            "WHERE 0 = (SELECT COUNT(*) FROM phones WHERE id=?)";
+
+    private final static String SQL_UPDATE_PHONE_QUERY =
+            "UPDATE phones " +
+            "SET brand=?, " +
+            "model=?, " +
+            "price=?, " +
+            "displaySizeInches=?, " +
+            "weightGr=?, " +
+            "lengthMm=?, " +
+            "widthMm=?, " +
+            "heightMm=?, " +
+            "announced=?, " +
+            "deviceType=?, " +
+            "os=?, " +
+            "displayResolution=?, " +
+            "pixelDensity=?, " +
+            "displayTechnology=?, " +
+            "backCameraMegapixels=?, " +
+            "frontCameraMegapixels=?, " +
+            "ramGb=?, " +
+            "internalStorageGb=?, " +
+            "batteryCapacityMah=?, " +
+            "talkTimeHours=?, " +
+            "standByTimeHours=?, " +
+            "bluetooth=?, " +
+            "positioning=?, " +
+            "imageUrl=?, " +
+            "description=? " +
+            "WHERE id=?";
 
     public Optional<Phone> get(final Long key) {
-        Phone phone = jdbcTemplate.queryForObject(
-                SQL_GET_PHONE_QUERY,
-                new Object[]{key},
-                new BeanPropertyRowMapper<>(Phone.class)
-        );
+        List<Phone> result = queryPhone(key);
 
-        if (phone != null){
+        Optional<Phone> phoneOptional;
+        if(result.size() == 0){
+            phoneOptional = Optional.empty();
+        } else {
+            Phone phone = result.get(0);
             phone.setColors(queryColors(key));
+            phoneOptional = Optional.of(result.get(0));
         }
 
-        return Optional.of(phone);
+        return phoneOptional;
+    }
+
+    private List<Phone> queryPhone(final Long key) {
+        return jdbcTemplate.query(
+                SQL_GET_PHONE_QUERY,
+                new BeanPropertyRowMapper<>(Phone.class),
+                key
+        );
     }
 
     private Set<Color> queryColors(final Long key) {
         return new HashSet<>(jdbcTemplate.query(
                 SQL_GET_COLORS_QUERY,
-                new Object[]{key},
                 (resultSet, i) -> {
                     Color color = new Color();
                     color.setCode(resultSet.getString("code"));
                     color.setId(resultSet.getLong("id"));
                     return color;
-                }
+                },
+                key
         ));
     }
 
     public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+        jdbcTemplate.update(SQL_INSERT_PHONE_IF_NOT_EXIST_QUERY,
+                phone.getId(),
+                phone.getBrand(),
+                phone.getModel(),
+                phone.getId()
+        );
+
+        jdbcTemplate.update(SQL_UPDATE_PHONE_QUERY,
+                phone.getBrand(),
+                phone.getModel(),
+                phone.getPrice(),
+                phone.getDisplaySizeInches(),
+                phone.getWeightGr(),
+                phone.getLengthMm(),
+                phone.getWidthMm(),
+                phone.getHeightMm(),
+                phone.getAnnounced(),
+                phone.getDeviceType(),
+                phone.getOs(),
+                phone.getDisplayResolution(),
+                phone.getPixelDensity(),
+                phone.getDisplayTechnology(),
+                phone.getBackCameraMegapixels(),
+                phone.getFrontCameraMegapixels(),
+                phone.getRamGb(),
+                phone.getInternalStorageGb(),
+                phone.getBatteryCapacityMah(),
+                phone.getTalkTimeHours(),
+                phone.getStandByTimeHours(),
+                phone.getBluetooth(),
+                phone.getPositioning(),
+                phone.getImageUrl(),
+                phone.getDescription(),
+                phone.getId()
+        );
     }
 
     public List<Phone> findAll(int offset, int limit) {
