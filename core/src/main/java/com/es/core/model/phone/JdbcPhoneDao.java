af90ae44d82d7ae01634f6f5bf9b80b5b1984dc6
Vladislav <cameroncoob@gmail.com>
2018-03-10 22:53:17 +0300

    Task 1

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -1,10 +1,19 @@
 package com.es.core.model.phone;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,16 +21,90 @@ import java.util.Optional;
 public class JdbcPhoneDao implements PhoneDao{
     @Resource
     private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert insertPhone;
+
+    @Resource
+    public void setDataSource(DataSource dataSource){
+        insertPhone = new SimpleJdbcInsert(dataSource).withTableName("phones").usingGeneratedKeyColumns("id");
+    }
 
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        Phone phone = (Phone)jdbcTemplate.queryForObject("select * from phones where phones.id = " + key,
+                new BeanPropertyRowMapper(Phone.class));
+        setColor(phone);
+        return Optional.of(phone);
     }
 
     public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+        long amount = jdbcTemplate.queryForObject("select count(*) from phones where phones.id = " + phone.getId(),
+                Long.class);
+        if (amount == 0) {
+            insertPhone(phone);
+        }
+        else {
+            updatePhone(phone);
+        }
     }
 
+    private void insertPhone(final Phone phone){
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(phone);
+        Long phoneId = insertPhone.executeAndReturnKey(parameters).longValue();
+        phone.setId(phoneId);
+        insertColors(phone);
+    }
+
+    private void insertColors(final Phone phone){
+        jdbcTemplate.batchUpdate("insert into phone2color (phoneId, colorId) values (?, ?)", new BatchPreparedStatementSetter() {
+
+            List<Color> colors = new ArrayList<>(phone.getColors());
+
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException
+            {
+                preparedStatement.setLong(1, phone.getId());
+                preparedStatement.setLong(2, colors.get(i).getId());
+            }
+
+            @Override
+            public int getBatchSize()
+            {
+                return colors.size();
+            }
+        });
+    }
+
+    private void updatePhone(final Phone phone) {
+        jdbcTemplate.update(SqlQueryConstants.SQL_QUERY_PHONE_UPDATE + phone.getId(),
+                phone.getBrand(), phone.getModel(), phone.getPrice(), phone.getDisplaySizeInches(),
+                phone.getWeightGr(), phone.getLengthMm(), phone.getWidthMm(), phone.getHeightMm(),
+                phone.getAnnounced(), phone.getDeviceType(), phone.getOs(), phone.getDisplayResolution(),
+                phone.getPixelDensity(), phone.getDisplayTechnology(), phone.getBackCameraMegapixels(),
+                phone.getFrontCameraMegapixels(), phone.getRamGb(), phone.getInternalStorageGb(), phone.getBatteryCapacityMah(),
+                phone.getTalkTimeHours(), phone.getStandByTimeHours(), phone.getBluetooth(), phone.getPositioning(),
+                phone.getImageUrl(), phone.getDescription());
+        updateColors(phone);
+    }
+
+    private void updateColors(final Phone phone) {
+        jdbcTemplate.update("delete from phone2color where phone2color.phoneId = " + phone.getId());
+        insertColors(phone);
+    }
+
+
     public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        List<Phone> phones = jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit,
+                new BeanPropertyRowMapper(Phone.class));
+        for (Phone phone : phones) {
+            setColor(phone);
+        }
+        return phones;
+    }
+
+    private void setColor(final Phone phone) {
+        Long phoneId = phone.getId();
+        List<Color> colors = jdbcTemplate.query("select * from colors inner join phone2color " +
+                "on colors.id = phone2color.colorId where phone2color.phoneId = " + phoneId,
+                new BeanPropertyRowMapper(Color.class));
+        phone.setColors(new HashSet<>(colors));
     }
 }
---
+++ core/src/main/java/com/es/core/model/phone/SqlQueryConstants.java
@@ -0,0 +1,31 @@
+package com.es.core.model.phone;
+
+public interface SqlQueryConstants {
+    public static final String SQL_QUERY_PHONE_UPDATE = "update phones set" +
+            " brand = ?," +
+            " model = ?," +
+            " price = ?," +
+            " displaySizeInches = ?," +
+            " weightGr = ?," +
+            " lengthMm = ?," +
+            " widthMm = ?," +
+            " heightMm = ?," +
+            " announced = ?," +
+            " deviceType = ?," +
+            " os = ?," +
+            " displayResolution = ?," +
+            " pixelDensity = ?," +
+            " displayTechnology = ?," +
+            " backCameraMegapixels = ?," +
+            " frontCameraMegapixels = ?," +
+            " ramGb = ?," +
+            " internalStorageGb = ?," +
+            " batteryCapacityMah = ?," +
+            " talkTimeHours = ?," +
+            " standByTimeHours = ?," +
+            " bluetooth = ?," +
+            " positioning = ?," +
+            " imageUrl = ?," +
+            " description = ?" +
+            " where phones.id = ";
+}
