70d036017f9d55e0918469fe2c5889b337a97967
Olga Shiryaeva <algorithmhell@gmail.com>
2018-03-10 20:58:41 +0300

    Task 2.2

--- web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
@@ -19,18 +19,6 @@ public class ProductListPageController {
     @RequestMapping(method = RequestMethod.GET)
     public String showProductList(Model model) {
         model.addAttribute("phones", phoneDao.findAll(0, 10));
-        System.out.println("This is done in order to check if the implementation of JdbcPhoneDao truly works : \n" +
-                "Will be removed in the the next commit ");
-        Phone phone = phoneDao.get(1000L).get();
-        System.out.println(phone.getBrand() + " works "  + phone.getId());
-        phone.setDescription("JJJJ");
-        phoneDao.save(phone);
-        phone.setId(2000L);
-        phone.setBrand("KLK");
-        phone.setModel("KKK");
-        phoneDao.save(phone);
-        System.out.println(phoneDao.get(1000L).get().getDescription() + " works "  + phone.getId());
-        System.out.println(phoneDao.get(2000L).get().getBrand() + " works "  + phone.getId());
         return "productList";
     }
 }
