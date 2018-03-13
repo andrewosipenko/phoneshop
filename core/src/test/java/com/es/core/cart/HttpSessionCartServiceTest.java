83a0000f9767087483211408dddfe83cf955660b
alastifer <grishkevichaaa@gmail.com>
2018-03-13 16:43:51 +0300

    Implemented tests for cart and cost services

--- core/src/test/java/com/es/core/cart/HttpSessionCartServiceTest.java
+++ core/src/test/java/com/es/core/cart/HttpSessionCartServiceTest.java
@@ -2,112 +2,218 @@ package com.es.core.cart;
 
 import com.es.core.model.phone.Phone;
 import com.es.core.model.phone.PhoneService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static util.PhoneUtils.createPhone;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:testContext.xml")
+@RunWith(MockitoJUnitRunner.class)
 public class HttpSessionCartServiceTest {
-    @Resource
-    private HttpSession httpSession;
+    @InjectMocks
+    private HttpSessionCartService httpSessionCartService = new HttpSessionCartService();
 
-    @Resource
-    private CartService cartService;
+    @Mock
+    private Cart cartMock;
 
-    @Resource
+    @Mock
     private PhoneService phoneService;
 
+    @Mock
+    private HttpSession httpSessionMock;
+
+    private List<Phone> phoneList;
+    private static final int PHONE_COUNT = 5;
     private static final String ATTRIBUTE_CART = "cart";
+    private static final long NONEXISTENT_PHONE = -1;
 
-    @Test
-    public void checkGetCartWithoutCartInSession() {
-        assertEquals(httpSession.getAttribute(ATTRIBUTE_CART), cartService.getCart());
+    @Before
+    public void initMocks() {
+        initPhoneList();
+        initPhoneServiceMock();
+        initCartMock();
+        when(httpSessionMock.getAttribute(ATTRIBUTE_CART)).thenReturn(cartMock);
+    }
+
+    private void initPhoneList() {
+        phoneList = new ArrayList<>();
+        for (int i = 1; i <= PHONE_COUNT; i++) {
+            phoneList.add(createPhone(i));
+        }
+    }
+
+    private void initPhoneServiceMock() {
+        phoneList.forEach(phone -> when(phoneService.get(phone.getId())).thenReturn(Optional.of(phone)));
+        when(phoneService.get(NONEXISTENT_PHONE)).thenReturn(Optional.empty());
+    }
+
+    private void initCartMock() {
+        when(cartMock.getItems()).thenReturn(new HashMap<>());
     }
 
     @Test
-    public void checkGetCartWithCartInSession() {
-        Cart cart = new Cart();
-        Phone phone = phoneService.get(1000L).get();
-        cart.addItem(phone, 1L);
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        assertEquals(cart.getPrice(), cartService.getPrice());
+    public void checkAddOnePhoneTwice() {
+        final long COUNT_PHONES = 1;
+        final Long QUANTITY = 2L;
+        Phone phone = phoneList.get(0);
+
+        assertTrue(cartMock.getItems().isEmpty());
+
+        httpSessionCartService.addPhone(phone.getId(), 1L);
+        httpSessionCartService.addPhone(phone.getId(), 1L);
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        assertTrue(cartMock.getItems().keySet().contains(phone));
+        assertEquals(QUANTITY, cartMock.getItems().get(phone));
     }
 
     @Test
-    public void checkPrice() {
-        Cart cart = mock(Cart.class);
-        BigDecimal price = new BigDecimal("100");
-        when(cart.getPrice()).thenReturn(price);
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        assertEquals(price, cartService.getPrice());
-        verify(cart).getPrice();
-    }
-
-    @Test
-    public void checkAddPhoneToCart() {
-        Cart cart = mock(Cart.class);
-        Phone phone = phoneService.get(1000L).get();
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        cartService.addPhone(phone.getId(), 1L);
-        verify(cart).addItem(phone, 1L);
-    }
-
-    @Test
-    public void checkRemovePhoneFromCart() {
-        Cart cart = mock(Cart.class);
-        Phone phone = phoneService.get(1000L).get();
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        cartService.remove(phone.getId());
-        verify(cart).removeItem(phone);
-    }
-
-    @Test
-    public void checkCountItemsInCart() {
-        Cart cart = mock(Cart.class);
-        long countItems = 10;
-        when(cart.countItems()).thenReturn(countItems);
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        assertEquals(countItems, cartService.getCountItems());
-        verify(cart).countItems();
-    }
-
-    @Test
-    public void checkGetListOfItemsFromCart() {
-        Cart cart = mock(Cart.class);
-        Map<Phone, Long> phonesMap = new HashMap<Phone, Long>() {{
-            put(phoneService.get(1000L).get(), 1L);
-            put(phoneService.get(1001L).get(), 2L);
-            put(phoneService.get(1002L).get(), 2L);
+    public void checkAddTwoPhones() {
+        final long COUNT_PHONES = 2;
+        final long QUANTITY = 2L;
+        List<Phone> phonesForAdd = new ArrayList<Phone>() {{
+            add(phoneList.get(0));
+            add(phoneList.get(1));
         }};
 
-        List<Phone> phonesList = new LinkedList<>();
-        phonesList.add(phoneService.get(1000L).get());
-        phonesList.add(phoneService.get(1001L).get());
-        phonesList.add(phoneService.get(1001L).get());
-        phonesList.add(phoneService.get(1002L).get());
-        phonesList.add(phoneService.get(1002L).get());
+        assertTrue(cartMock.getItems().isEmpty());
 
-        when(cart.getItems()).thenReturn(phonesMap);
-        httpSession.setAttribute(ATTRIBUTE_CART, cart);
-        for(Phone phone : cartService.getAllItems()) {
-            assertTrue(String.format("Phone with id %d not contain in list", phone.getId()), phonesList.contains(phone));
-            phonesList.remove(phone);
-        }
-        assertTrue("Phone list contains more items then expected", phonesList.isEmpty());
-        verify(cart).getItems();
+        httpSessionCartService.addPhone(phonesForAdd.get(0).getId(), 1L);
+        httpSessionCartService.addPhone(phonesForAdd.get(1).getId(), 1L);
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        checkPhonesContainInCart(phonesForAdd, cartMock.getItems());
+        assertEquals(QUANTITY, httpSessionCartService.getCountItems());
+    }
+
+    @Test
+    public void checkAddOnePhone() {
+        final long COUNT_PHONES = 1;
+        final Long QUANTITY = 2L;
+        Phone phone = phoneList.get(0);
+
+        assertTrue(cartMock.getItems().isEmpty());
+
+        httpSessionCartService.addPhone(phone.getId(), 2L);
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        assertTrue(cartMock.getItems().keySet().contains(phone));
+        assertEquals(QUANTITY, cartMock.getItems().get(phone));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void checkAddNonexistentPhone() {
+        httpSessionCartService.addPhone(NONEXISTENT_PHONE, 1L);
+    }
+
+    @Test
+    public void checkAddTwoPhoneThenRemoveOne() {
+        final long COUNT_PHONES = 1;
+        List<Phone> phonesForAdd = new ArrayList<Phone>() {{
+            add(phoneList.get(0));
+            add(phoneList.get(1));
+        }};
+
+        httpSessionCartService.addPhone(phonesForAdd.get(0).getId(), 1L);
+        httpSessionCartService.addPhone(phonesForAdd.get(1).getId(), 1L);
+        checkPhonesContainInCart(phonesForAdd, cartMock.getItems());
+
+        httpSessionCartService.remove(phonesForAdd.get(0).getId());
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        assertTrue(cartMock.getItems().keySet().contains(phonesForAdd.get(1)));
+    }
+
+    @Test
+    public void checkAddPhoneThenRemoveThenAdd() {
+        final long COUNT_PHONES = 1;
+        Phone phone = phoneList.get(0);
+
+        httpSessionCartService.addPhone(phone.getId(), 1L);
+        assertTrue(cartMock.getItems().keySet().contains(phone));
+        httpSessionCartService.remove(phone.getId());
+        assertTrue(cartMock.getItems().isEmpty());
+        httpSessionCartService.addPhone(phone.getId(), 1L);
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        assertTrue(cartMock.getItems().keySet().contains(phone));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void checkRemoveNonexistentPhone() {
+        httpSessionCartService.remove(NONEXISTENT_PHONE);
+    }
+
+    @Test
+    public void checkUpdateOnePhoneWhenTwoInCart() {
+        final long COUNT_PHONES = 2;
+        final long QUANTITY = 3L;
+        List<Phone> phonesForAdd = new ArrayList<Phone>() {{
+            add(phoneList.get(0));
+            add(phoneList.get(1));
+        }};
+        Map<Long, Long> items = new HashMap<Long, Long>() {{put(1L, 2L);}};
+
+        httpSessionCartService.addPhone(phonesForAdd.get(0).getId(), 1L);
+        httpSessionCartService.addPhone(phonesForAdd.get(1).getId(), 1L);
+        checkPhonesContainInCart(phonesForAdd, cartMock.getItems());
+
+        httpSessionCartService.update(items);
+        assertEquals(COUNT_PHONES, cartMock.getItems().size());
+        checkPhonesContainInCart(phonesForAdd, cartMock.getItems());
+        assertEquals(QUANTITY, httpSessionCartService.getCountItems());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void checkUpdateNonexistentPhone() {
+        Map<Long, Long> items = new HashMap<Long, Long>(){{put(NONEXISTENT_PHONE, 1L);}};
+        httpSessionCartService.update(items);
+    }
+
+    @Test
+    public void checkItemsCountWhenAddTwoPhone() {
+        final long QUANTITY = 2L;
+        List<Phone> phonesForAdd = new ArrayList<Phone>() {{
+            add(phoneList.get(0));
+            add(phoneList.get(1));
+        }};
+
+        httpSessionCartService.addPhone(1L, 1L);
+        httpSessionCartService.addPhone(2L, 1L);
+        checkPhonesContainInCart(phonesForAdd, cartMock.getItems());
+        assertEquals(QUANTITY, httpSessionCartService.getCountItems());
+    }
+
+    @Test
+    public void checkItemsCountWhenOnePhoneTwice() {
+        final long QUANTITY = 2L;
+        httpSessionCartService.addPhone(1L, 1L);
+        httpSessionCartService.addPhone(1L, 1L);
+        assertEquals(QUANTITY, httpSessionCartService.getCountItems());
+    }
+
+    @Test
+    public void checkItemsCountWhenNoPhoneInCart() {
+        assertEquals(0, httpSessionCartService.getCountItems());
+    }
+
+    @Test
+    public void checkGetItems() {
+        Map<Phone, Long> items = phoneList.stream().collect(Collectors.toMap(phone -> phone, phone -> 1L));
+        phoneList.forEach(phone -> httpSessionCartService.addPhone(phone.getId(), 1L));
+        assertEquals(items, httpSessionCartService.getAllItems());
+    }
+
+    private void checkPhonesContainInCart(List<Phone> phonesForAdd, Map<Phone, Long> items) {
+        List phones = items.keySet().stream()
+                .filter(phone -> !phonesForAdd.contains(phone))
+                .collect(toList());
+        assertTrue(phones.isEmpty());
     }
 }
---
+++ core/src/test/java/com/es/core/cart/cost/HttpSessionCostServiceTest.java
@@ -0,0 +1,55 @@
+package com.es.core.cart.cost;
+
+import com.es.core.cart.HttpSessionCartService;
+import com.es.core.model.phone.Phone;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static util.PhoneUtils.createPhone;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HttpSessionCostServiceTest {
+    @InjectMocks
+    private HttpSessionCostService httpSessionCostService;
+
+    @Mock
+    private HttpSessionCartService httpSessionCartServiceMock;
+
+    private List<Phone> phoneList;
+    private static final int PHONE_COUNT = 5;
+
+    @Before
+    public void initPhoneList() {
+        phoneList = new ArrayList<>();
+        for (int i = 1; i <= PHONE_COUNT; i++) {
+            phoneList.add(createPhone(i));
+        }
+    }
+
+    @Test
+    public void checkCostWhenNoItemsInCart() {
+        when(httpSessionCartServiceMock.getAllItems()).thenReturn(new HashMap<>());
+        assertEquals(BigDecimal.ZERO, httpSessionCostService.getCost());
+    }
+
+    @Test
+    public void checkCostWhenItemsInCart() {
+        final BigDecimal COST = BigDecimal.valueOf(1500D);
+        Map<Phone, Long> items = phoneList.stream().collect(Collectors.toMap(phone -> phone, phone -> 1L));
+        when(httpSessionCartServiceMock.getAllItems()).thenReturn(items);
+        assertEquals(COST, httpSessionCostService.getCost());
+    }
+}
