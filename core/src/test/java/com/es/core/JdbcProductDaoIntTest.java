af31f6315f0ac139dfbf54b940b373917a5a0c07
Vladislav <cameroncoob@gmail.com>
2018-03-15 17:58:44 +0300

    Task 2.3

---
+++ core/src/test/java/com/es/core/JdbcProductDaoIntTest.java
@@ -0,0 +1,76 @@
+package com.es.core;
+
+import com.es.core.model.phone.Phone;
+import com.es.core.model.phone.PhoneDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Timed;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "/context/testContext-core.xml")
+public class JdbcProductDaoIntTest{
+    @Resource
+    PhoneDao phoneDao;
+
+    private static long EXISTING_PHONE_ID = 1000L;
+    private static long NOT_EXISTING_PHONE_ID = 1500L;
+    private static int PHONE_AMOUNT = 8;
+
+    @Test
+    public void testAddPhone(){
+        Phone phone = new Phone();
+        phone.setBrand("Brrrr");
+        phone.setModel("Mllll");
+        phoneDao.save(phone);
+        Assert.assertTrue(phone.getId() == 1009L);
+    }
+
+    @Test
+    public void testGetExistingPhone(){
+        Optional<Phone> optionalPhone = phoneDao.get(NOT_EXISTING_PHONE_ID);
+        Assert.assertFalse(optionalPhone.isPresent());
+    }
+
+    @Test
+    public void testGetNotExistingPhone(){
+        Optional<Phone> optionalPhone = phoneDao.get(EXISTING_PHONE_ID);
+        Assert.assertTrue(optionalPhone.isPresent());
+        Assert.assertTrue(EXISTING_PHONE_ID == optionalPhone.get().getId());
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Phone> phoneList = phoneDao.findAll(0, PHONE_AMOUNT);
+        Assert.assertTrue(phoneList.size() == PHONE_AMOUNT);
+    }
+
+    @Test
+    public void saveExistingPhone(){
+        Phone phone = phoneDao.get(EXISTING_PHONE_ID).get();
+        String oldModel = phone.getModel();
+        String newModel = oldModel + "newModel";
+        phone.setModel(newModel);
+        phoneDao.save(phone);
+        phone = phoneDao.get(phone.getId()).get();
+        Assert.assertTrue(phone.getModel().equals(newModel));
+    }
+
+    @Test
+    public void saveNotExistingPhone(){
+        Phone phone = new Phone();
+        String model = "checkModel";
+        String brand = "checkBrand";
+        phone.setModel(model);
+        phone.setBrand(brand);
+        phoneDao.save(phone);
+        Assert.assertEquals(model, phoneDao.get(phone.getId()).get().getModel());
+        Assert.assertEquals(brand, phoneDao.get(phone.getId()).get().getBrand());
+    }
+}
