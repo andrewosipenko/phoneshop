04ffeb1bde9fd6c168a6e2624a6e4971a1425254
Vladislav <cameroncoob@gmail.com>
2018-04-11 20:17:27 +0300

    Code refactoring Refactored error handling for invalid quantity

---
+++ core/src/main/java/com/es/core/model/phone/exception/InvalidAddToCartFormException.java
@@ -0,0 +1,7 @@
+package com.es.core.model.phone.exception;
+
+public class InvalidAddToCartFormException extends Exception {
+    public InvalidAddToCartFormException(String message){
+        super(message);
+    }
+}
