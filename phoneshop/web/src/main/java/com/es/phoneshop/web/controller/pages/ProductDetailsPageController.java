a47e7ea8609c20b7df1eabf8b136b9909578191c
M1santhrop <dm.timofeev.ol@gmail.com>
2018-11-22 01:20:05 +0300

    After code review

--- phoneshop/web/src/main/java/com/es/phoneshop/web/controller/pages/ProductDetailsPageController.java
+++
@@ -1,9 +0,0 @@
-package com.es.phoneshop.web.controller.pages;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = "/productDetails")
-public class ProductDetailsPageController {
-}
