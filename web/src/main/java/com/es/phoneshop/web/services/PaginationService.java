d9124e6a175473c4c297082136553e066627a652
M1santhrop <dm.timofeev.ol@gmail.com>
2018-12-25 16:14:27 +0300

    fix bugs

--- web/src/main/java/com/es/phoneshop/web/services/PaginationService.java
+++ web/src/main/java/com/es/phoneshop/web/services/PaginationService.java
@@ -1,14 +1,15 @@
 package com.es.phoneshop.web.services;
 
-import com.es.core.model.phone.PhoneDao;
-import com.es.phoneshop.web.controller.exceptions.InvalidUrlException;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.es.core.dao.phone.PhoneDao;
+import com.es.phoneshop.web.controller.exceptions.InvalidParametersInUrlException;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class PaginationService {
 
-    @Autowired
+    @Resource
     private PhoneDao phoneDao;
 
     public static final int PHONES_TO_DISPLAY = 10;
@@ -16,12 +17,12 @@ public class PaginationService {
 
     public int getPageStartNumber(Integer pageNumber, String search) {
         if (pageNumber < 1) {
-            throw new InvalidUrlException("Invalid page number: " + pageNumber);
+            throw new InvalidParametersInUrlException("Invalid page number: " + pageNumber);
         }
         pageNumber = getAvailableNewPage(pageNumber, search);
         return (pageNumber % PAGES_NUMBER != 0 || pageNumber == 0)
                 ? pageNumber - (pageNumber % PAGES_NUMBER) + 1
-                : pageNumber - PAGES_NUMBER;
+                : pageNumber - PAGES_NUMBER + 1;
     }
 
     public int getAvailableNewPage(Integer pageNumber, String search) {
@@ -29,13 +30,23 @@ public class PaginationService {
         return (pageNumber > maxPageNumber) ? maxPageNumber : pageNumber;
     }
 
-    public int getMaxPageNumber(String search) {
-        int maxPageNumber = (int) Math.ceil((double) (phoneDao.getNumberAvailablePhones(search) / PHONES_TO_DISPLAY));
-        return maxPageNumber == 0 ? 1 : maxPageNumber;
-    }
-
     public int getAmountPagesToDisplay(Integer startPage, String search) {
         int maxPageNumber = getMaxPageNumber(search);
         return (maxPageNumber - startPage) > PAGES_NUMBER ? PAGES_NUMBER : maxPageNumber - startPage + 1;
+    }
+
+    public int getMaxPageNumber(String search) {
+        int maxPageNumber = (int) Math.ceil((double) (phoneDao.getNumberAvailablePhones(search) / PHONES_TO_DISPLAY));
+        return maxPageNumber != 0 ? maxPageNumber : 1;
+    }
+
+    public int getNewPage(Integer currentPage, String action, String search) {
+        if (action.equals("next")) {
+            return getPageStartNumber(currentPage + PAGES_NUMBER, search);
+        } else if (action.equals("prev")) {
+            return (currentPage - PAGES_NUMBER < 1) ? currentPage : currentPage - PAGES_NUMBER;
+        } else {
+            throw new InvalidParametersInUrlException("Action " + action + " is wrong!");
+        }
     }
 }
