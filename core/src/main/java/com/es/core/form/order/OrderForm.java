b49d9864a9681f7231da42c150e0c769ee12096a
M1santhrop <dm.timofeev.ol@gmail.com>
2019-01-03 15:07:12 +0300

    Task 2.7-2.8: add forms

--- core/src/main/java/com/es/core/form/order/OrderForm.java
+++ core/src/main/java/com/es/core/form/order/OrderForm.java
@@ -1,4 +1,91 @@
 package com.es.core.form.order;
 
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
 public class OrderForm {
+    private List<OrderFormItem> orderFormItems;
+    private String firstName;
+    private String lastName;
+    private String deliveryAddress;
+    private String contactPhoneNo;
+    private String additionalInformation;
+    private BigDecimal subtotal;
+    private BigDecimal deliveryPrice;
+    private BigDecimal totalPrice;
+
+    public List<OrderFormItem> getOrderFormItems() {
+        return orderFormItems;
+    }
+
+    public void setOrderFormItems(List<OrderFormItem> orderFormItems) {
+        this.orderFormItems = orderFormItems;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getContactPhoneNo() {
+        return contactPhoneNo;
+    }
+
+    public void setContactPhoneNo(String contactPhoneNo) {
+        this.contactPhoneNo = contactPhoneNo;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getDeliveryPrice() {
+        return deliveryPrice;
+    }
+
+    public void setDeliveryPrice(BigDecimal deliveryPrice) {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
