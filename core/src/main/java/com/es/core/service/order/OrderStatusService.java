3c086586e2177a9aef63e4a1c2824b238c308bda
M1santhrop <dm.timofeev.ol@gmail.com>
2019-01-28 04:39:18 +0300

    Task 2.8 - 2.9: implement new services

--- core/src/main/java/com/es/core/service/order/OrderStatusService.java
+++ core/src/main/java/com/es/core/service/order/OrderStatusService.java
@@ -1,4 +1,9 @@
 package com.es.core.service.order;
 
+import com.es.core.model.order.OrderStatus;
+
+import java.util.List;
+
 public interface OrderStatusService {
+    List<String> getOrderStatusNamesExcept(OrderStatus orderStatus);
 }
