3aaf5920dc760cbde92f4e40e0d2bf314a98f860
SashaKatchinsky <46282602+SashaKatchinsky@users.noreply.github.com>
2019-07-18 11:52:18 +0300

    Add ProductDao with JdbcProductDao, database properties from property file, tests for JdbcProducrDao and ProductListPageController

--- core/src/main/java/com/es/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/es/core/order/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
     @Override
     public Order createOrder(Cart cart) {
         throw new UnsupportedOperationException("TODO");
