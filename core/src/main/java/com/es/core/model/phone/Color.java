499b0cc1b3a67c54bb48875ef5642e38fca9102c
SashaKatchinsky <46282602+SashaKatchinsky@users.noreply.github.com>
2019-07-19 22:47:56 +0300

    Revert "Revert "Revert "Add ProductDao with JdbcProductDao, database properties from property file, tests for JdbcProducrDao and ProductListPageController"""
    This reverts commit ae46149cf1ad8b6505f541bea13cbc27603f3fa8.

--- core/src/main/java/com/es/core/model/phone/Color.java
+++ core/src/main/java/com/es/core/model/phone/Color.java
@@ -4,14 +4,6 @@ public class Color {
     private Long id;
     private String code;
 
-    public Color() {
-    }
-
-    public Color(Long id, String code) {
-        this.id = id;
-        this.code = code;
-    }
-
     public Long getId() {
         return id;
     }
@@ -26,23 +18,5 @@ public class Color {
 
     public void setCode(final String code) {
         this.code = code;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Color color = (Color) o;
-
-        if (id != null ? !id.equals(color.id) : color.id != null) return false;
-        return code != null ? code.equals(color.code) : color.code == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (code != null ? code.hashCode() : 0);
-        return result;
     }
 }
--- web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
@@ -1,30 +1,23 @@
 package com.es.phoneshop.web.controller.pages;
 
-import com.es.core.model.ProductDao;
-import com.es.core.model.phone.PhoneDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Resource;
+import com.es.core.model.phone.PhoneDao;
 
 @Controller
 @RequestMapping (value = "/productList")
 public class ProductListPageController {
-
     @Resource
     private PhoneDao phoneDao;
 
-    @Autowired
-    private ProductDao productDao;
-
     @RequestMapping(method = RequestMethod.GET)
     public String showProductList(Model model) {
-        productDao.load(1010L);
         model.addAttribute("phones", phoneDao.findAll(0, 10));
         return "productList";
     }
-
 }
