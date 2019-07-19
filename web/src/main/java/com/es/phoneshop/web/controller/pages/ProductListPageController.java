db878d9ea68ef3d859b05920626c088f58d8d352
SashaKatchinsky <46282602+SashaKatchinsky@users.noreply.github.com>
2019-07-19 22:48:10 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Add ProductDao with JdbcProductDao, database properties from property file, tests for JdbcProducrDao and ProductListPageController""""""""
    This reverts commit 25c6f09828a677b4023dda8adb856de8083aff96.

--- web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
@@ -1,23 +1,30 @@
 package com.es.phoneshop.web.controller.pages;
 
-import javax.annotation.Resource;
-
+import com.es.core.model.ProductDao;
+import com.es.core.model.phone.PhoneDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.es.core.model.phone.PhoneDao;
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping (value = "/productList")
 public class ProductListPageController {
+
     @Resource
     private PhoneDao phoneDao;
 
+    @Autowired
+    private ProductDao productDao;
+
     @RequestMapping(method = RequestMethod.GET)
     public String showProductList(Model model) {
+        productDao.load(1010L);
         model.addAttribute("phones", phoneDao.findAll(0, 10));
         return "productList";
     }
+
 }
