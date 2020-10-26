54ecdd08bf8ef2ac713238694c6bd1693a47ba53
VladBrown <45466811+VladikBrown@users.noreply.github.com>
2020-10-26 10:24:21 +0300

    Revert "Revert "Check point 1""

---
+++ core/src/main/java/com/es/core/model/DAO/phone/PhoneDao.java
@@ -0,0 +1,12 @@
+package com.es.core.model.DAO.phone;
+
+import com.es.core.model.entity.phone.Phone;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PhoneDao {
+    Optional<Phone> get(Long key);
+    void save(Phone phone);
+    List<Phone> findAll(int offset, int limit);
+}
