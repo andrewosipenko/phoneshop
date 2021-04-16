e6ca7896dc6746114ecfb36955e558bd35a16ab8
Nastassia Katsuba <nastassia.katsuba@gmail.com>
2021-04-16 20:41:05 +0300

    Implement JdbcPhoneDao

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -9,19 +9,44 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class JdbcPhoneDao implements PhoneDao{
+public class JdbcPhoneDao implements PhoneDao {
+    private static final String PHONE_BY_ID = "select * from phones p left join phone2color p2c on p.id = p2c.phoneId left join colors c on p2c.colorId = c.id where p.id = ?";
+    private static final String ALL_PHONES = "select * from (select * from phones limit ? offset ?) p left join phone2color p2c on p.id = p2c.phoneId left join colors c on p2c.colorId = c.id";
+    private static final String INSERT_PHONE = "insert into phones values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_PHONE = "update phones set brand = ?, model = ?, price = ?, displaySizeInches = ?, weightGr = ?, lengthMm = ?, widthMm = ?, heightMm = ?, announced = ?, deviceType = ?, os = ?, displayResolution = ?, pixelDensity = ?, displayTechnology = ?, backCameraMegapixels = ?, frontCameraMegapixels = ?, ramGb = ?, internalStorageGb = ?, batteryCapacityMah = ?, talkTimeHours = ?, standByTimeHours = ?, bluetooth = ?, positioning = ?, imageUrl = ?, description = ? where id = ?";
+    private static final String PHONE2COLOR_BY_PHONE = "select * from phone2color where phoneId = ?";
+    private static final String INSERT_PHONE2COLOR = "insert into phone2color values (?, ?)";
+    private static final String DELETE_PHONE2COLOR = "delete from phone2color where phoneId = ? and colorId = ?";
+
     @Resource
     private JdbcTemplate jdbcTemplate;
 
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        return jdbcTemplate.query(PHONE_BY_ID, new PhoneResultSetExtractor(), key).stream().findAny();
     }
 
     public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+        Optional<Phone> optionalPhone = get(phone.getId());
+        if (optionalPhone.isPresent()) {
+            jdbcTemplate.update(UPDATE_PHONE, phone.getBrand(), phone.getModel(), phone.getPrice(), phone.getDisplaySizeInches(), phone.getWeightGr(), phone.getLengthMm(), phone.getWidthMm(), phone.getHeightMm(), phone.getAnnounced(), phone.getDeviceType(), phone.getOs(), phone.getDisplayResolution(), phone.getPixelDensity(), phone.getDisplayTechnology(), phone.getBackCameraMegapixels(), phone.getFrontCameraMegapixels(), phone.getRamGb(), phone.getInternalStorageGb(), phone.getBatteryCapacityMah(), phone.getTalkTimeHours(), phone.getStandByTimeHours(), phone.getBluetooth(), phone.getPositioning(), phone.getImageUrl(), phone.getDescription(), phone.getId());
+            List<Color> colors = jdbcTemplate.query(PHONE2COLOR_BY_PHONE, new BeanPropertyRowMapper<>(Color.class), phone.getId());
+            colors.forEach(c -> {
+                if (!phone.getColors().contains(c)) {
+                    jdbcTemplate.update(DELETE_PHONE2COLOR, phone.getId(), c.getId());
+                }
+            });
+            phone.getColors().forEach(c -> {
+                if (!colors.contains(c)) {
+                    jdbcTemplate.update(INSERT_PHONE2COLOR, phone.getId(), c.getId());
+                }
+            });
+        } else {
+            jdbcTemplate.update(INSERT_PHONE);
+            phone.getColors().forEach(c -> jdbcTemplate.update(INSERT_PHONE2COLOR, phone.getId(), c.getId()));
+        }
     }
 
     public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        return jdbcTemplate.query(ALL_PHONES, new PhoneResultSetExtractor(), limit, offset);
     }
 }
---
+++ core/src/main/java/com/es/core/model/phone/PhoneResultSetExtractor.java
@@ -0,0 +1,75 @@
+package com.es.core.model.phone;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PhoneResultSetExtractor implements ResultSetExtractor<List<Phone>> {
+    @Override
+    public List<Phone> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+        List<Phone> phones = new ArrayList<>();
+        while (resultSet.next()) {
+            Phone phone = new Phone();
+            Long id = resultSet.getLong("id");
+            phone.setId(id);
+            Color color = findColor(resultSet);
+            Optional<Phone> optionalPhone = phones.stream().filter(p -> p.getId().equals(id)).findAny();
+            if (optionalPhone.isPresent()) {
+                int phoneIndex = phones.stream()
+                        .map(Phone::getId)
+                        .collect(Collectors.toList())
+                        .indexOf(id);
+                if (color.getCode() != null) {
+                    phones.get(phoneIndex).getColors().add(color);
+                }
+            } else {
+                setFieldValues(phone, resultSet);
+                if (color.getCode() != null) {
+                    Set<Color> colorSet = new HashSet<>(Arrays.asList(color));
+                    phone.setColors(colorSet);
+                }
+                phones.add(phone);
+            }
+        }
+        System.out.println(phones);
+        return phones;
+    }
+
+    private void setFieldValues(Phone phone, ResultSet resultSet) throws SQLException, DataAccessException {
+        phone.setBrand(resultSet.getString("brand"));
+        phone.setModel(resultSet.getString("model"));
+        phone.setPrice(resultSet.getBigDecimal("price"));
+        phone.setDisplaySizeInches(resultSet.getBigDecimal("displaySizeInches"));
+        phone.setWeightGr(resultSet.getInt("weightGr"));
+        phone.setLengthMm(resultSet.getBigDecimal("lengthMm"));
+        phone.setWidthMm(resultSet.getBigDecimal("widthMm"));
+        phone.setHeightMm(resultSet.getBigDecimal("heightMm"));
+        phone.setAnnounced(resultSet.getDate("announced"));
+        phone.setDeviceType(resultSet.getString("deviceType"));
+        phone.setOs(resultSet.getString("os"));
+        phone.setDisplayResolution(resultSet.getString("displayResolution"));
+        phone.setPixelDensity(resultSet.getInt("pixelDensity"));
+        phone.setDisplayTechnology(resultSet.getString("displayTechnology"));
+        phone.setBackCameraMegapixels(resultSet.getBigDecimal("backCameraMegapixels"));
+        phone.setFrontCameraMegapixels(resultSet.getBigDecimal("frontCameraMegapixels"));
+        phone.setRamGb(resultSet.getBigDecimal("ramGb"));
+        phone.setInternalStorageGb(resultSet.getBigDecimal("internalStorageGb"));
+        phone.setBatteryCapacityMah(resultSet.getInt("batteryCapacityMah"));
+        phone.setTalkTimeHours(resultSet.getBigDecimal("talkTimeHours"));
+        phone.setStandByTimeHours(resultSet.getBigDecimal("standByTimeHours"));
+        phone.setBluetooth(resultSet.getString("bluetooth"));
+        phone.setPositioning(resultSet.getString("positioning"));
+        phone.setImageUrl(resultSet.getString("imageUrl"));
+        phone.setDescription(resultSet.getString("description"));
+    }
+
+    private Color findColor(ResultSet resultSet) throws SQLException, DataAccessException {
+        Long colorId = resultSet.getLong("colorId");
+        String colorCode = resultSet.getString("code");
+        return new Color(colorId, colorCode);
+    }
+}
