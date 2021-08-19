200952ea53e88d67fc95066529462f7999f080bf
Artsiom <galaxyfame.02235@gmail.com>
2021-08-19 12:29:57 +0300

    CHECKPOINT 3

--- web/src/main/test/java/com/es/phoneshop/web/conf/TestConfig.java
+++ web/src/main/test/java/com/es/phoneshop/web/conf/TestConfig.java
@@ -1,6 +1,12 @@
 package com.es.phoneshop.web.conf;
 
+import com.es.core.service.cart.CartService;
+import com.es.core.service.cart.HttpSessionCartService;
 import com.es.core.service.phone.PhoneStockService;
+import com.es.core.service.phone.ProductPageService;
+import com.es.core.service.phone.impl.ProductPageServiceImpl;
+import com.es.phoneshop.web.controller.pages.ProductListPageController;
+import com.es.phoneshop.web.controller.pages.cart.AddToCartValidator;
 import com.es.phoneshop.web.controller.pages.cart.UpdateCartValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +24,25 @@ public class TestConfig {
     @Bean
     public PhoneStockService phoneStockService() {
         return mock(PhoneStockService.class);
+    }
+
+    @Bean
+    public AddToCartValidator addToCartValidator() {
+        return new AddToCartValidator();
+    }
+
+    @Bean
+    public ProductListPageController productListPageController() {
+        return new ProductListPageController();
+    }
+
+    @Bean
+    public ProductPageService productPageService() {
+        return mock(ProductPageService.class);
+    }
+
+    @Bean
+    public CartService cartService() {
+        return mock(CartService.class);
     }
 }
